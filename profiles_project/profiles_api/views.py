cf9e42bc329f7445744885dfc0101a7f74245929
Mohammad Natour <natour@gmail.com>
2020-07-04 17:25:42 +0400

    Add new API APIView

---
+++ profiles_project/profiles_api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is simial to a traditional Django View',
+            'Gives you the the most control over your app logigice',
+            'Is mapped manually to URLs'
+        ]
+        return Response({'message': "Hello", 'an_apiview': an_apiview})
